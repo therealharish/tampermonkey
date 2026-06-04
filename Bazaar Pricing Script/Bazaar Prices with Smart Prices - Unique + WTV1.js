43603ebf66441db2a8b9ef1c048621ee3833a93c
Mohd Harish <harishh@cisco.com>
2026-06-04 15:37:01 +0530

    change

--- Bazaar Pricing Script/Bazaar Prices with Smart Prices - Unique + WTV1.js
+++ Bazaar Pricing Script/Bazaar Prices with Smart Prices - Unique + WTV1.js
@@ -2,7 +2,7 @@
 // @name          Bazaar Filler With Smart Pricing - Unique
 // @namespace     http://tampermonkey.net/
 // @version       6.2
-// @author        WTV1
+// @author        Unique
 // @description   Advanced Bazaar Filler with Market and Bazaar Price Points + Fill All + Trade Fill + Visual Qty and Price currently on Bazaar
 // @match         https://www.torn.com/bazaar.php*
 // @match         https://www.torn.com/trade.php*
